23ce110addb5eb3045c81424b4c4039e13fd2438
did <diarselimi92@gmail.com>
2022-11-16 09:51:50 +0100

    fix width

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,8 @@ export default function Home(props) {
                   I have worked with veraiety of different companies, and I have had experiences on some really interesting applications.
               </p>
           </div>
-          <div className="columns-2 md:columns-2">
-            <div className="p-5 w-full pt-12">
+            <div className="w-2/6">
+             <div className="p-5 w-full pt-12">
                 {posts.map((post, index) => (
                     <ArticlePreview post={post} slug={post.slug} key={post.key} />
                 ))}
