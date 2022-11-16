bf83729409f94d1bad18010d1a414ab8948e6885
did <diarselimi92@gmail.com>
2022-11-16 09:15:22 +0100

    add tags

---
+++ pages/tags.js
@@ -0,0 +1,30 @@
+
+export default function Tags(props) {
+    const {tags} = props
+    return (
+        <div className="container">
+            {tags.map((tag) => (
+                    <a href={tag} >#{tag}</a>
+                    ))}
+        </div>
+    )
+}
+
+export async function getStaticProps() {
+    const files = fs.readdirSync("posts");
+
+    const tags = [];
+    const posts = files.map((file, key) => {
+        const slug = file.replace(".md", "");
+        const content = fs.readFileSync(`posts/${file}`, "utf-8");
+        const parsedContent = matter(content);
+
+        tags.concat(parsedContent.tags)
+    });
+
+    return {
+        props: {
+            tags
+        }
+    }
+}
