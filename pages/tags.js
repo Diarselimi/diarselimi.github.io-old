a52b166db795822180e88bd746cc72dfb0305835
did <diarselimi92@gmail.com>
2022-11-16 11:01:51 +0100

    finally

--- pages/tags.js
+++ pages/tags.js
@@ -1,22 +1,42 @@
-export default function Tags({tags}) {
+import fs from 'fs';
+import matter from "gray-matter";
+import ReactMarkdown from 'react-markdown';
+
+export default function Tags({posts}) {
 
     return (
         <div className="container">
             <h3> Tags </h3>
-            {tags.map((tag, index) => (
-                    <div key={tag} >
-                        <a href={tag} >#{tag}</a>
-                    </div>
-                    ))}
+            {posts.map((post) => (
+                    post.data.tags.map((tag, index) => (
+                        <div key={tag} >
+                            <a href={tag} >#{tag}</a>
+                        </div>
+                    ))
+            ))}
         </div>
     )
 }
 
 export async function getStaticProps() {
+    const files = fs.readdirSync("posts");
+
+    const posts = files.map((file, key) => {
+        const slug = file.replace(".md", "");
+        const content = fs.readFileSync(`posts/${file}`, "utf-8");
+        const parsedContent = matter(content);
+
+        const {data} = parsedContent;
+        return {
+            key,
+            slug,
+            data
+        }
+    });
 
     return {
         props: {
-            tags : ["go", "php", "learning", "mutex"]
+            posts
         }
     }
 }
